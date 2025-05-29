1b73cfb69b829f117cf3a5c620b6705f24c9c06c
depermana12 <deddiapermana97@gmail.com>
2025-05-29 22:54:28 +0700

    add cli: setup args

---
+++ cli.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+def cli_scraper_parser():
+    parser = argparse.ArgumentParser(
+        prog="Jobstreet scraper",
+        description="Scrape applied jobs from JobStreet and export it to json, csv, excell",
+    )
+
+    parser.add_argument("-e", "--email", type=str, help="Your jobstreet email address")
+
+    parser.add_argument(
+        "-m",
+        "--max",
+        type=str,
+        default="1",
+        help="Number of pages to scrap. use --all to scrape all the available page",
+    )
+
+    parser.add_argument(
+        "-x",
+        "--export",
+        type=str,
+        choices=["json", "csv", "excel"],
+        default="json",
+        help="Export format for the scraped data",
+    )
